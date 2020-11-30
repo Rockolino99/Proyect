af9edd8cdd0ba56621b628b4c203ad0654e118a8
Karime <karilomeli267@gmail.com>
2020-11-30 10:17:06 -0600

    Update main.php

--- views/main.php
+++ views/main.php
@@ -12,23 +12,23 @@
             <div class="card-body">
                 <h5 class="card-title">Invierno</h5>
                 <p class="card-text">Colección de temporada, que el clima no sea un impedimento para que tu puedas lucir perfecta.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
         <div class="card" style="width: 33.33%;">
             <img src="images/img2.jpg" class="card-img-top" title="Vestidos">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Vestidos</h5>
+                <p class="card-text">Colección sorprendente, encontraras un vestido ideal para cualquier ocasión y hará brillar como siempre.</p>
+                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
         <div class="card" style="width: 33.33%;">
             <img src="images/img3.jpg" class="card-img-top" title="Disney">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">Disney</h5>
+                <p class="card-text">Colección especial, que mejor manera de lucir bien en conjunto con tus personajes favoritos.</p>
+                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
     </div>
