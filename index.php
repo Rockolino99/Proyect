4d808fc1b7ef28f8ea9b0f7aadd20a925b4574b6
Rockolino99 <rickymuro9@gmail.com>
2020-11-29 19:04:10 -0600

    Estilos2

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda | Cutsie Girl</title>
     <!--Links-->
-    <link rel="stylesheet" href="views/estilos.css">
-    <link rel="stylesheet" href="estilos2.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="stylesheet" href="css/estilos2.css">
     <!--Fin Links-->
 </head>
 <body>
