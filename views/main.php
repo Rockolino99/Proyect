81bca8331ba21a7a233a9126386c3c30f694bc05
Rockolino99 <rickymuro9@gmail.com>
2020-11-29 20:00:42 -0600

    Varios

--- views/main.php
+++ views/main.php
@@ -1,38 +1,26 @@
-<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-<ol class="carousel-indicators">
-  <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-  <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-  <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-</ol>
-<div class="carousel-inner">
-  <div class="carousel-item active">
-    <img src="images/img3.jpg" class="d-block w-50" alt="..." id="car">
-    <div class="carousel-caption d-none d-md-block">
-      <h5>First slide label</h5>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+<div class="content">
+    <div class="card" style="width: 33.33%;">
+        <img src="images/img1.jpg" class="card-img-top" title="Invierno">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
-  </div>
-  <div class="carousel-item">
-    <img src="images/img2.jpg" class="d-block w-100" alt="..." id="car">
-    <div class="carousel-caption d-none d-md-block">
-      <h5>Second slide label</h5>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div class="card" style="width: 33.33%;">
+        <img src="images/img2.jpg" class="card-img-top"  title="Vestidos">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
-  </div>
-  <div class="carousel-item">
-    <img src="images/img3.jpg" class="d-block w-100" alt="..." id="car">
-    <div class="carousel-caption d-none d-md-block">
-      <h5>Third slide label</h5>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    <div class="card" style="width: 33.33%;">
+        <img src="images/img3.jpg" class="card-img-top"  title="Disney">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
     </div>
-  </div>
-</div>
-<a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
-</a>
 </div>
