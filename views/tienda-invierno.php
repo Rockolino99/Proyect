02a86d13c6ec6623f57319adc80ffa8701087b18
lalo0099 <lalopenaesparza@gmail.com>
2020-12-06 20:05:01 -0600

    Create tienda-invierno.php

---
+++ views/tienda-invierno.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="col-md-12">
+        <div class="page-header">
+            <h1 class="h1-certificado">Invierno<small>xD</small></h1>
+        </div>
+    </div>
+</body>
+
+</html>
