7df1946cdc98869aac3abf907b04b67e82047a53
Rockolino99 <rickymuro9@gmail.com>
2020-12-06 21:43:01 -0600

    Avance
    Avance

--- views/main.php
+++ views/main.php
@@ -7,27 +7,30 @@
 <body>
     <div class="content">
         <div class="card" style="width: 33.33%;">
-            <img src="images/img1.jpg" class="card-img-top" title="Invierno">
+            <div class="opaco">
+                <img src="images/img1.jpg" class="card-img-top" title="Invierno">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">Invierno</h5>
                 <p class="card-text">Colección de temporada, que el clima no sea un impedimento para que tu puedas lucir perfecta.</p>
-                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
         <div class="card" style="width: 33.33%;">
-            <img src="images/img2.jpg" class="card-img-top" title="Vestidos">
+            <div class="opaco">
+                <img src="images/img2.jpg" class="card-img-top" title="Vestidos">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">Vestidos</h5>
                 <p class="card-text">Colección sorprendente, encontraras un vestido ideal para cualquier ocasión y hará brillar como siempre.</p>
-                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
         <div class="card" style="width: 33.33%;">
-            <img src="images/img3.jpg" class="card-img-top" title="Disney">
+            <div class="opaco">
+                <img src="images/img3.jpg" class="card-img-top" title="Disney">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">Disney</h5>
                 <p class="card-text">Colección especial, que mejor manera de lucir bien en conjunto con tus personajes favoritos.</p>
-                <a href="#" class="btn btn-primary">Compra ya!</a>
             </div>
         </div>
     </div>
