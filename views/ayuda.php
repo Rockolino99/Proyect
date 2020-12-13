e4c92f77fd267d51bd2146817c44559e5438dcb0
Karime <karilomeli267@gmail.com>
2020-12-13 17:48:24 -0600

    Update ayuda.php

--- views/ayuda.php
+++ views/ayuda.php
@@ -9,14 +9,16 @@
                 <div class="card-header" id="headingOne">
                     <h5 class="mb-0">
                         <button class="btn btn-link" style="color: black; padding:10px;" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Pregunta #1
+                            ¿Por qué mi pedido se retrasa?
                         </button>
                     </h5>
                 </div>
 
                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        Debido a la pandemía del covid-19 las paqueterias estan trabajando más lento y nosotros con menos personal es por eso que se acumula el trabajo y lo que antes nos tomaba algunas horas para hacer ahora nos toma días. <br>
+                        No te preocupes tu pedido llegará tal vez no tan rápido como estabas acostumbrada a que lo hiciera pero llegara. <br>
+                        Cutsie Girl respeta la seguridad de todas las personas- Bajo esta situación, es posible que su pedido sufra un retraso de 4-5 días.
                     </div>
                 </div>
             </div>
@@ -24,13 +26,14 @@
                 <div class="card-header" id="headingTwo">
                     <h5 class="mb-0">
                         <button style="color: black; padding:10px;" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                            Pregunta #2
+                            ¿Cuánto tarda el envío?
                         </button>
                     </h5>
                 </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        Cada paquete tiene un tiempo estimado de entrega diferente basado en el lugar en la fila que tienes y si tenemos tus productos en almacen o están por llegar. <br>
+                        Además de eso, la lejanía en la que estemos cuenta mucho y la paquetería por la que es enviado. 
                     </div>
                 </div>
             </div>
@@ -38,13 +41,13 @@
                 <div class="card-header" id="headingThree">
                     <h5 class="mb-0">
                         <button style="color: black; padding:10px;" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            Pregunta #3
+                            ¿Cuánto cuesta el envío?
                         </button>
                     </h5>
                 </div>
                 <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        El envío depende de la cantidad de tu compra y del peso de este, puede ser gratis si tu compra es gratis o si tenemos alguna promoción vigente, también depende de la ciudad de donde solicites nuestros servicios y la paquetería que prefieras.
                     </div>    
                 </div>
             </div>
@@ -52,13 +55,21 @@
                 <div class="card-header" id="headingFour">
                     <h5 class="mb-0">
                         <button style="color: black; padding:10px;" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            Pregunta #4
+                            ¿Se permiten devoluciones?
                         </button>
                     </h5>
                 </div>
                 <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    Si no has quedado 100% satisfecha/o, dispones de 30 días después de la fecha de recepción del pedido para realizar la devolución. no se aceptarán devoluciones realizadas 30 días después de la fecha de recepción.
+
+​<br><br>Los gastos de envío de la devolución no son gratuitos. Puedes elegir un método de devolución por tu cuenta y gasto, pero no aceptamos pagos a contra reembolso. 
+
+​
+
+<br><br>Solo puedes devolver artículos sin uso, sin lavados, sin daños, con las etiquetas originales y en su embalaje original. 
+
+<br><br>Los siguientes artículos no pueden ser devueltos: bodys, ropa interior, artículos de belleza y ofertas. 
                     </div>    
                 </div>
             </div>
@@ -66,13 +77,15 @@
                 <div class="card-header" id="headingFive">
                     <h5 class="mb-0">
                         <button style="color: black; padding:10px;" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                            Pregunta #5
+                            ¿Cómo puedo ponerme en contacto si tengo alguna duda?
                         </button>
                     </h5>
                 </div>
                 <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        Puedes escribirnos en la pestaña "Contáctanos" tenemos un equipo encargado de resolver tus dudas y que seguramente lo hara con mucho gusto. <br>
+                        También nos puedes escribir en cualquiera de nuestras redes sociales. <br>
+                        Será un gusto atenderte.
                     </div>    
                 </div>
             </div>
