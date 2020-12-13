4eb6cfd9726f9610986a612ae96138a30a42152f
Karime <karilomeli267@gmail.com>
2020-12-13 14:05:53 -0600

    Update acercade.php

--- views/acercade.php
+++ views/acercade.php
@@ -5,21 +5,27 @@
 <div class="invierno">
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:45%;">
     <h3 style="text-align:center;">INVIERNO</h3>
-    <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+    <p>¿Quién no ha sentido que no puede tener un equilibrio entre la temporada y su outfit?<br>
+      Invierno es una colección temporal que se adapta a tus necesidades, nace con el único propósito de hacerte lucir comoda con la temporada y hacerte lucir bien.
+      <br>Invierno aparece año con año con prendas que te van a encantar y portaras con orgullo, nació hace algunos años cuando nos dimos cuenta que todos merecemos estilo y amor en nuestros outfits de frío.
+      <br>Visita nuestra colección, enamorate junto con nosotros de todo lo que Invierno tiene para ti, lleno de amor, colores y buenas vibras.
+    </p>
     <div style="text-align:center;">
-      <button type="button" class="btn btn-outline-dark">Compra ya!</button>
+      <button type="button" class="btn btn-outline-dark">Comprar ya...</button>
     </div>
   </div>
-  <div class="bgimg-1" style="background-image: url('images/invierno.jpg'); min-height: 800px; width:55%;"></div>
+  <div class="bgimg-1" style="background-image: url('images/outfitin.jpg'); min-height: 800px; width:55%;"></div>
 </div>
 
 <div class="vestidos">
-  <div class="bgimg-2" style="background-image: url('images/vestidos.jpg'); min-height: 800px; width:60%;"></div>
+  <div class="bgimg-2" style="background-image: url('images/ves.jpg'); min-height: 800px; width:60%;"></div>
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:40%;">
     <h3 style="text-align:center;">VESTIDOS</h3>
-    <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+    <p>Vestidos es una colección que siempre vas a encontrar activa y actualizada, todo el año trabajamos en ella adaptandola a lo último de la moda y a la temporada en la que nos encontramos. <br>
+  En ella encontraras todo lo que buscas ya que su principal objetivo es darte el vestido de tus sueños y acompañarte a donde quiera que vayas con mucho estilo <br>
+Queremos que cualquier evento al que asistas nos lleves contigo ya que llevaras un pedacito de nuestro corazón que se encuentra en esta colección llena de amor, colores y buenas vibras.</p>
     <div style="text-align:center;">
-      <button type="button" class="btn btn-outline-dark">Compra ya!</button>
+      <button type="button" class="btn btn-outline-dark">Comprar ya...</button>
     </div>
   </div>
 </div>
@@ -27,11 +33,14 @@
 <div class="disney">
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:45%;">
     <h3 style="text-align:center;">DISNEY</h3>
-    <p>Parallax scrolling is a web site trend where the background content is moved at a different speed than the foreground content while scrolling. Nascetur per nec posuere turpis, lectus nec libero turpis nunc at, sed posuere mollis ullamcorper libero ante lectus, blandit pellentesque a, magna turpis est sapien duis blandit dignissim. Viverra interdum mi magna mi, morbi sociis. Condimentum dui ipsum consequat morbi, curabitur aliquam pede, nullam vitae eu placerat eget et vehicula. Varius quisque non molestie dolor, nunc nisl dapibus vestibulum at, sodales tincidunt mauris ullamcorper, dapibus pulvinar, in in neque risus odio. Accumsan fringilla vulputate at quibusdam sociis eleifend, aenean maecenas vulputate, non id vehicula lorem mattis, ratione interdum sociis ornare. Suscipit proin magna cras vel, non sit platea sit, maecenas ante augue etiam maecenas, porta porttitor placerat leo.</p>
+    <p>Disney es nuestra colección especial y la única que encontraras en el mercado. <br>
+  Nace por mi amor a los personajes que marcaron mi infancia y que seguramente la tuya también, nace con una idea loca y divertida de hacer que lleves a esos personajes contigo pero siempre luciendo bien <br>
+Disney se mantiene actualizada todo el año con prendas únicas para ti.<br>
+Te invitamos a visitar y enamorarte de esta colección única en el mercado, llena de amor, buenas vibras y colores.</p>
     <div style="text-align:center;">
-      <button type="button" class="btn btn-outline-dark">Compra ya!</button>
+      <button type="button" class="btn btn-outline-dark">Comprar ya...</button>
     </div>
   </div>
-  <div class="bgimg-3" style="background-image: url('images/disney.jpg'); min-height: 800px; width:55%;"></div>
+  <div class="bgimg-3" style="background-image: url('images/fondodisney.jpg'); min-height: 800px; width:55%;"></div>
 </div>
 
