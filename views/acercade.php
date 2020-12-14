15a69ea41111eed63c68362063252ba5208dfa9c
GiovannaZapata <75220780+GiovannaZapata@users.noreply.github.com>
2020-12-13 23:27:20 -0600

    Update acercade.php

--- views/acercade.php
+++ views/acercade.php
@@ -5,9 +5,9 @@
 <div class="invierno">
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:45%;">
     <h3 style="text-align:center;">MISIÓN</h3>
-    <p>
+    <p><br>
       Nuestra principal misión es darte un outfit digno de ti, que puedas salir a la calle con alguna de nuestras prendas y sepas que luces bien y sobre todo que estas comoda. <br>
-      Es complicado dar un balance entre la ropa comoda y la ropa a la moda, nosotros tratando de dar ese balance creamos nuestras tres categorías donde seguramente encontrarás algo que se adapte a ti, a tus gustos y a tus necesidades.
+      <br>Es complicado dar un balance entre la ropa comoda y la ropa a la moda, nosotros tratando de dar ese balance creamos nuestras tres categorías donde seguramente encontrarás algo que se adapte a ti, a tus gustos y a tus necesidades.
       Cada una de las prendas esta llena de amor, colores y buenas vibras para ti, con todo nuestro corazón hasta la puerta de tu casa.
     </p>
   </div>
@@ -17,10 +17,10 @@
 <div class="vestidos">
   <div class="bgimg-2" style="background-image: url('images/ves.jpg'); min-height: 800px; width:60%;"></div>
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:40%;">
-    <h3 style="text-align:center;">VISIÓN.</h3>
-    <p>
+    <h3 style="text-align:center;">VISIÓN</h3>
+    <p><br>
       Uno de nuestros anhelos más grandes es que Cutsie Girl se convierta en tu tienda favorita, que necesites cualquier prenda y la primer tienda en la que pienses que lo podrás conseguir seamos nosotros. <br>
-      Queremos llegar a tener una tienda física donde puedas asistir con tu familia o amigos y tengas una atención digna de ti. <br>
+      <br>Queremos llegar a tener una tienda física donde puedas asistir con tu familia o amigos y tengas una atención digna de ti. <br><br>
       Esperamos llegar a ser tu tienda favorita que te conviertas en nuestra amiga y siempre seamos tu primer opción.
     </p>
   </div>
@@ -29,10 +29,10 @@
 <div class="disney">
   <div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify; width:45%;">
     <h3 style="text-align:center;">NUESTO OBJETIVO</h3>
-    <p>
-      Nuestro objetivo más grande es hacerte lucir bien, con estilo y radiante a cualquier lugar que vayas, acompañarte en cualquier ocasión y te sientas bien de llevarnos contigo.
+    <p><br>
+      Nuestro objetivo más grande es hacerte lucir bien, con estilo y radiante a cualquier lugar que vayas, acompañarte en cualquier ocasión y te sientas bien de llevarnos contigo. <br>
     <br>Queremos que Cutsie Girl no sea solo una tienda para ti, si no también un hogar donde encuentres amigas que te guiaran para realizar compras exitosas y que en cada colección encuentres algo que se adapta a ti 
-    en todos los sentidos<br>
+    en todos los sentidos.<br><br>
     Somos una tienda llena de amor donde ponemos el corazón en cada paquete procesado.
     </p>
   </div>
