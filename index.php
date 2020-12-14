f845376d3d46622374845a694cee57dd4a1a3811
Karime <karilomeli267@gmail.com>
2020-12-14 14:10:18 -0600

    Revert "Merge branch 'main' of https://github.com/Rockolino99/Proyect into main"
    This reverts commit 8a5bc09be90bd37f32606fa2e4d786b6a03f0fd0, reversing
    changes made to 96aceed24c511be9004df44f9ee18caf30268ea5.

--- index.php
+++ index.php
@@ -195,30 +195,29 @@ session_start();
                         <div class="row">
                             <div class="col">
                                 <label for="nombreRestablecer">Correo electrónico:</label>
-                                <input type="text" class="form-control" id="nombreReestablecer" placeholder="Correo Electrónico">
+                                <input type="text" class="form-control" id="nombreRestablecer" placeholder="Correo Electrónico">
                             </div>
                         </div>
                         <br>
                         <div class="row">
                             <div class="col">
                                 <label for="passRestablecer1">Contraseña:</label>
-                                <input type="password" class="form-control" id="passReestablecer1" placeholder="Contraseña">
+                                <input type="password" class="form-control" id="passRestablecer1" placeholder="Contraseña">
                             </div>
                             <div class="col">
                                 <label for="passRestablecer2">Repetir contraseña:</label>
-                                <input type="password" class="form-control" id="passReestablecer2" placeholder="Repetir Contraseña">
+                                <input type="password" class="form-control" id="passRestablecer2" placeholder="Repetir Contraseña">
                             </div>
                         </div>
                         <br><br>
                         <center>
-                            <button type="button" class="btn btn-primary" onclick="reestablecerPass()">Cambiar contraseña</button>
+                            <button class="btn btn-primary" onclick="reestablecerPass()">Cambiar contraseña</button>
                         </center>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    <!--Fin Modal Reestablecer Contraseña-->
     <!--Fin Header-->
     <?php
     $mod = (isset($_GET['mod'])) ? $_GET['mod'] : "main";
