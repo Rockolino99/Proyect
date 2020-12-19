129ac812441fa2faceeb5c85f62e16da7830bbae
moni12raid <moni12raid@gmail.com>
2020-12-18 18:36:25 -0600

    Update main.php
    se agregó iconos a las palabras invierno, disney y vestidos

--- views/main.php
+++ views/main.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
 </head>
 
 <body>
@@ -24,7 +25,8 @@
                 <img src="images/img1.jpg" class="card-img-top" title="Invierno">
             </div>
             <div class="card-body">
-                <h5 class="card-title">Invierno</h5>
+                <h5 class="card-title"><i class="far fa-snowflake"></i>  Invierno</h5>
+
                 <p class="card-text">Es una colección que aparece año con año, llena de cosas nuevas y a la moda que seguramente te harán lucir perfecta. <br>
                     Quien dijo que el clima es un impendimento para tener estilo y portarlo a donde quiera que vayas. <br>
                     Visita nuestra colección llena de amor para ti y enamórate de ella.</p>
@@ -35,7 +37,7 @@
                 <img src="images/img2.jpg" class="card-img-top" title="Vestidos">
             </div>
             <div class="card-body">
-                <h5 class="card-title">Vestidos</h5>
+                <h5 class="card-title"><i class="fas fa-tshirt"></i>  Vestidos</h5>
                 <p class="card-text">Tu colección favorita y donde siempre encontrarás lo que buscas. <br>
                     Encuentra aquí un outfit para cualquier ocasión desde lo más casual hasta lo más formal y en cualquiera te hará lucir preciosa. <br>
                     Conoce nuestra coleccion llena de buenas vibras.</p>
@@ -46,34 +48,34 @@
                 <img src="images/img3.jpg" class="card-img-top" title="Disney">
             </div>
             <div class="card-body">
-                <h5 class="card-title">Disney</h5>
+                <h5 class="card-title"><i class="fas fa-child"></i>  Disney</h5>
                 <p class="card-text">Nuestra colección especial y única en el mercado. <br>
                     Llego a revolucionar la manera de vestir llevando contigo a tus personajes favoritos de una manera divertida y sin perder el estilo que te caracteriza.</p>
             </div>
         </div>
     </div>
-    <div id="cupones" >
-        <section class="contenedor" >
+    <div id="cupones">
+        <section class="contenedor">
             <br><br>
-            <h2 class="centrar-texto" >CUPÓN DEL MES</h2>
+            <h2 class="centrar-texto">CUPÓN DEL MES</h2>
             <br>
-            <h3 class="centrar-texto">Llegaron las ofertas Navideñas</h3> 
+            <h3 class="centrar-texto">Llegaron las ofertas Navideñas</h3>
             <br>
             <div class="centrar-texto">
-            <img  src="images/CUPONES/propuestas a modificar/Cupon 40off.png" alt="cupón">
+                <img src="images/CUPONES/propuestas a modificar/Cupon 40off.png" alt="cupón">
             </div>
             <br><br>
             <h3 class="centrar-texto">Suscríbete para obtener más cupónes</h3>
             <br>
             <fieldset class="centrar-texto">
-                    <label for="email">E-mail:</label>
-                    <input class="buu" type="email" id="email" placeholder="Tu correo electrónico" required>
+                <label for="email">E-mail:</label>
+                <input class="buu" type="email" id="email" placeholder="Tu correo electrónico" required>
             </fieldset>
             <br>
             <input type="submit" value="Suscribirme" class="boton boton-azul "><br><br><br>
 
             <br><br>
-          
+
         </section>
     </div>
 
