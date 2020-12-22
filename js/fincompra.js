0e60641a1e5b8934402f25cde3ac6a8e58b0ea0c
Karime <karilomeli267@gmail.com>
2020-12-21 21:26:32 -0600

    Validaciones formulario de pago

--- js/fincompra.js
+++ js/fincompra.js
@@ -1,6 +1,7 @@
 function finalizarCompra() {
     var nombre = $('#name').val()
-   
+    var number = $('#number').val()
+    var codigo = $('#security').val()
 
     //validación nombre
     if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
@@ -11,6 +12,26 @@ function finalizarCompra() {
             timer: 2000
         })
         $('#InputNombre').focus()
+        return
+    }
+    if (number == null || number.length == 0 || number.length < 16 || number.length > 16) {
+        swal({
+            icon: 'warning',
+            text: '¡Ingresa una tarjeta valida!',
+            buttons: false,
+            timer: 2000
+        })
+        $('#number').focus()
+        return
+    }
+    if (codigo == null || codigo.length == 0 || codigo.length < 3 || codigo.length > 3) {
+        swal({
+            icon: 'warning',
+            text: '¡Ingresa un código valido!',
+            buttons: false,
+            timer: 2000
+        })
+        $('#security').focus()
         return
     }
 }
